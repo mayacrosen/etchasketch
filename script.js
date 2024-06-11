19d16097a3189c7a885809217c02e3c9c3c833ea
mayacrosen <157836637+mayacrosen@users.noreply.github.com>
2024-06-11 13:11:52 -0400

    Mostly works, grid is now based on input. Can only make on grid, if you enter another number it adds to the og grid. Need to fix it so it clear the old grid.

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const container = document.querySelector(".container")
+const enter = document.querySelector("button");
+const input = document.querySelector("input");
+let gridSize = 0;
 
-    for (let i=0; i<16; i++) {
+function drawGrid() {
+    const temp = parseInt(input.value);
+    if (temp < 100) {
+        gridSize = temp;
+    }
+    for (let i=0; i<gridSize; i++) {
         const row = document.createElement("div");
         row.className = "row";
         container.appendChild(row);
-        for (let n=0; n<16; n++) {
+        for (let n=0; n<gridSize; n++) {
             const item = document.createElement("div");
             item.className = "square";
             item.addEventListener("mouseover", function(){item.style.backgroundColor = "black";});
             row.appendChild(item);
         }
     }
+} 
+
+
+
+enter.addEventListener("click", drawGrid);
